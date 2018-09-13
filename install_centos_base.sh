36774e960349363ccd52ff51c5baa9144b73a896
icoolworld <278767718@qq.com>
2018-09-13 15:38:27 +0800

    Update install_centos_base.sh

--- install_centos_base.sh
+++ install_centos_base.sh
@@ -118,4 +118,13 @@ ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ""
 ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ""
 ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""
 
+# install docker
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+
 cd / && rm -rf /data
