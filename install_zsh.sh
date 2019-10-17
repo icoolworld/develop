85741906aabf9ca6c91819f32d15ec223255c7c7
baby <liujinhua_17173@cyou-inc.com>
2019-10-17 16:05:00 +0800

    zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -3,4 +3,9 @@ mkdir -p /data
 cd /data/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 \cp /build/.zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 source ~/.zshrc
+
+
