7f46e7c7ad4d9ca76264f8478ec86bc36a830d40
coolbaby <coolbaby>
2019-10-18 16:26:14 +0800

    auto confirm zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /data
 cd /data/
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) yes"
 \cp /build/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
