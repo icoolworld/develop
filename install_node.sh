48a5afce22038550bd6f50fdfdf0f527a85ade91
coolbaby <coolbaby>
2019-10-24 14:20:28 +0800

    node install

--- install_node.sh
+++ install_node.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 mkdir -p /data/ && cd /data/
 
-wget https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-x64.tar.xz
-tar xf node-v10.16.3-linux-x64.tar.xz
-cd node-v10.16.3-linux-x64/
+#curl -sL install-node.now.sh/lts | bash
+
+wget https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-x64.tar.xz
+tar xf node-v12.13.0-linux-x64.tar.xz
+cd node-v12.13.0-linux-x64/
 \cp -r bin/ include/ lib/ share/ /usr/
 
 #avaScript 代码规范，自带 linter & 代码自动修正
 npm install --global standard
 npm install --global prettier
 npm install --global eslint
+
+# install yarn
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
+yum install -y yarn
 
 cd / && rm -rf /data/
