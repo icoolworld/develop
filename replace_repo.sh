c965021fb3ba7b4d92084a66dc765215bd489b6e
coolbaby <coolbaby>
2019-10-31 13:15:40 +0800

    repo

--- replace_repo.sh
+++ replace_repo.sh
@@ -13,7 +13,12 @@ yum clean all
 
 # replace repo
 composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
-npm --registry https://registry.npm.taobao.org info underscore
+npm --registry https://registry.npm.taobao.org info underscore -g
+yarn config set registry https://registry.npm.taobao.org -g
+mkdir -p ~/.config/pip/
+touch ~/.config/pip/pip.conf
+echo '[global]
+index-url = https://mirrors.aliyun.com/pypi/simple/' >~/.config/pip/pip.conf
 go env -w GOPROXY=https://goproxy.cn,direct
 
 cd / && rm -rf /data
